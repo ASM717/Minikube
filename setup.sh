7e75a76c988c4a0751e1395cf4ac8a939e5be997
ASM717 <stepan151@gmail.com>
2021-04-06 21:00:24 +0300

    1 day

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: amuriel <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/06 13:32:23 by amuriel           #+#    #+#              #
-#    Updated: 2021/04/06 14:24:04 by amuriel          ###   ########.fr        #
+#    Updated: 2021/04/06 18:33:41 by amuriel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,8 +14,17 @@
 
 minikube start --vm-driver=virtualbox
 minikube addons enable dashboard
+minikube addons enable ingress
 minikube addons enable metallb
 
-minikube dashboard &
+#minikube dashboard
 eval $(minikube docker-env)
 
+
+kubectl apply -f srcs/metallb.yaml
+
+kubectl apply -f srcs/nginx/nginx.yaml
+
+sleep 20
+
+kubectl get pods
