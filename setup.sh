1c7f73606842e2c0bdc3f06462713f2aa68a7a29
ASM717 <stepan151@gmail.com>
2021-04-14 16:51:44 +0300

    mysql+php

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: amuriel <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/06 13:32:23 by amuriel           #+#    #+#              #
-#    Updated: 2021/04/09 18:38:19 by amuriel          ###   ########.fr        #
+#    Updated: 2021/04/14 12:51:45 by amuriel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,9 +24,14 @@ docker build -t nginx_image srcs/nginx
 kubectl apply -f srcs/metallb.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
 
-
 docker build -t phpmyadmin_image srcs/phpmyadmin
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+
+docker build -t mysql_image srcs/mysql
+kubectl apply -f ./srcs/mysql/mysql.yaml
+
+docker build -t wordpress_image srcs/wordpress
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
 # sleep 20
 
--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -29,12 +29,20 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = 'mysql-service';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+
+/* User used to manipulate with storage */
+//$cfg['Servers'][$i]['controlhost'] = '';
+$cfg['Servers'][$i]['port'] = '3306';
+$cfg['Servers'][$i]['controluser'] = 'root';
+$cfg['Servers'][$i]['controlpass'] = 'amuriel';
 
 /**
  * Directories for saving/loading files from server
  */
 $cfg['UploadDir'] = '';
 $cfg['SaveDir'] = '';
+
+$cfg['TempDir'] = '/tmp';
