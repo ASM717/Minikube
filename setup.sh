3dc0080f1674234b82b79e4e3ca163a2c056b3fa
ASM717 <stepan151@gmail.com>
2021-04-20 13:20:11 +0300

    ftps and other

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: amuriel <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/06 13:32:23 by amuriel           #+#    #+#              #
-#    Updated: 2021/04/14 12:51:45 by amuriel          ###   ########.fr        #
+#    Updated: 2021/04/20 12:19:24 by amuriel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,12 +16,12 @@ minikube start --vm-driver=virtualbox
 minikube addons enable dashboard
 minikube addons enable metallb
 minikube addons enable metrics-server
-minikube addons list
 
 eval $(minikube docker-env)
 
-docker build -t nginx_image srcs/nginx
 kubectl apply -f srcs/metallb.yaml
+
+docker build -t nginx_image srcs/nginx
 kubectl apply -f ./srcs/nginx/nginx.yaml
 
 docker build -t phpmyadmin_image srcs/phpmyadmin
@@ -33,7 +33,8 @@ kubectl apply -f ./srcs/mysql/mysql.yaml
 docker build -t wordpress_image srcs/wordpress
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
-# sleep 20
+docker build -t ftps_image srcs/ftps
+kubectl apply -f ./srcs/ftps/ftps.yaml
 
-# kubectl get pods
 minikube dashboard
+# kubectl get pods
--- srcs/mysql/init_start_db.sh
+++ srcs/mysql/init_start_db.sh
@@ -1,15 +1,28 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    startsupervisord.sh                                :+:      :+:    :+:    #
+#    init_start_db.sh                                   :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: amuriel <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/04/14 16:53:21 by amuriel           #+#    #+#              #
-#    Updated: 2021/04/14 16:53:22 by amuriel          ###   ########.fr        #
+#    Created: 2021/04/14 16:49:40 by amuriel           #+#    #+#              #
+#    Updated: 2021/04/20 12:54:04 by amuriel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
+
+mariadb-install-db --user=root
+
+mysqld --user=root & sleep 5
+
+mysql --user=root --execute="CREATE DATABASE wordpress;"
+
+mysql --user=root wordpress < mysql-service.sql
+
+mysql --user=root --execute="CREATE USER 'root'@'%' IDENTIFIED BY 'amuriel';
+							GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+							USE wordpress;
+							FLUSH PRIVILEGES;"
 
 /usr/bin/supervisord -c /etc/supervisord.conf
