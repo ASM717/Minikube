a3bd20cb200279957fc974f38c93eef595513043
ASM717 <stepan151@gmail.com>
2021-04-27 16:31:25 +0300

    pkill.sh

--- srcs/pkill.sh
+++ srcs/pkill.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# kubectl exec deploy/influxdb-deployment -- pkill influxd && kubectl exec deploy/influxdb-deployment -- pkill telegraf
+kubectl exec deploy/influxdb-deployment -- pkill influxd && kubectl exec deploy/influxdb-deployment -- pkill telegraf
 
-# kubectl exec deploy/nginx-deployment -- pkill nginx && kubectl exec deploy/nginx-deployment -- pkill telegraf
+kubectl exec deploy/nginx-deployment -- pkill nginx && kubectl exec deploy/nginx-deployment -- pkill telegraf
 
-# kubectl exec deploy/wordpress-deployment -- pkill nginx && kubectl exec deploy/wordpress-deployment -- pkill telegraf && kubectl exec deploy/wordpress-deployment -- pkill php-fpm
+kubectl exec deploy/wordpress-deployment -- pkill nginx && kubectl exec deploy/wordpress-deployment -- pkill telegraf && kubectl exec deploy/wordpress-deployment -- pkill php-fpm
 
-# kubectl exec deploy/phpmyadmin-deployment -- pkill nginx && kubectl exec deploy/phpmyadmin-deployment -- pkill telegraf && kubectl exec deploy/phpmyadmin-deployment -- pkill php-fpm
+kubectl exec deploy/phpmyadmin-deployment -- pkill nginx && kubectl exec deploy/phpmyadmin-deployment -- pkill telegraf && kubectl exec deploy/phpmyadmin-deployment -- pkill php-fpm
 
-# kubectl exec deploy/mysql-deployment -- pkill mysqld && kubectl exec deploy/mysql-deployment -- pkill telegraf
+kubectl exec deploy/mysql-deployment -- pkill mysqld && kubectl exec deploy/mysql-deployment -- pkill telegraf
 
-# kubectl exec deploy/ftps-deployment -- pkill vsftpd && kubectl exec deploy/ftps-deployment -- pkill telegraf
+kubectl exec deploy/ftps-deployment -- pkill vsftpd && kubectl exec deploy/ftps-deployment -- pkill telegraf
 
-# ubectl exec deploy/grafana-deployment -- pkill grafana && kubectl exec deploy/grafana-deployment -- pkill telegraf
+ubectl exec deploy/grafana-deployment -- pkill grafana && kubectl exec deploy/grafana-deployment -- pkill telegraf
 
